219419bc543519fb1c21a7700b2107763d44f1c4
SANTHK <ssdnsanth.kumar@gmail.com>
2017-01-05 18:32:34 +0530

    angular

---
+++ angular/constants/API.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Dell on 15-08-2016.
+ */
+
+app.constant('API', {
+    "ANR": "http://localhost:9000/",
+    "SMSAPI": "http://login.bulksmsgateway.in/sendmessage.php?user=SANTH&password=54321"
+});
